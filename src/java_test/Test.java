e07ac666d34566765645333fdb22c6c0b7708f77
601-5 <601-5@DESKTOP-RDUHP84>
2024-01-15 15:35:05 +0900

    7th commit

--- src/java_test/Test.java
+++ src/java_test/Test.java
@@ -2,9 +2,10 @@ package java_test;
 
 public class Test {
 
-	public static void main(String[] args) { //as,dsaaaaaaaaaaadfmldflsdfmlsdf;mlsdmkfdsfmo;dsfm;omsof;dm;omsd;fm;dm;ds;;flw;eml;mfewl;km sklmd fl;aml;mfweomq;
+	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요");
+
 	}
 
 }
