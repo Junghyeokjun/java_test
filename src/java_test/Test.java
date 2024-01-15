4f3991f8d74523961f56999327d8aa85e2784c99
Junghyeokjun <154102467+Junghyeokjun@users.noreply.github.com>
2024-01-15 15:14:08 +0900

    Update Test.java
    6th commit

--- src/java_test/Test.java
+++ src/java_test/Test.java
@@ -2,7 +2,7 @@ package java_test;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //as,dsaaaaaaaaaaadfmldflsdfmlsdf;mlsdmkfdsfmo;dsfm;omsof;dm;omsd;fm;dm;ds;;flw;eml;mfewl;km sklmd fl;aml;mfweomq;
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요");
 	}
